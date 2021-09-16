cced5b4bf0dcdd663cec7d48b5e4a7859de6700a
SravaniJalibili364 <89896513+SravaniJalibili364@users.noreply.github.com>
2021-09-16 15:20:00 +0530

    python

--- first_program.py
+++ first_program.py
@@ -1,17 +1,20 @@
-first_input = input()
-first_len = len(first_input)
-if (first_len%2 != 0):
-    middle_one = round((first_len/2))
+first_input = input()           # Taking the first String
+first_len = len(first_input)    # checking the length of the first string
+if (first_len%2 != 0):          # if length of the first string is odd or not
+    middle_one = round((first_len/2))   # if it is odd print the middle one
 else:
-    middle_one = round(first_len/2) + 1
+    middle_one = round(first_len/2) + 1         # if it is even it takes first middle one
 
 
-second_input = input()
-sec_len = len(second_input)
-if(sec_len % 2 != 0):
-    s_middle_one = round((sec_len/2))
+second_input = input()          # Taking the first String
+sec_len = len(second_input)     # checking the length of the first string
+if(sec_len % 2 != 0):           # if length of the first string is odd or not
+    s_middle_one = round((sec_len/2))   #if it is odd print the middle one
 else:
-    s_middle_one = round(sec_len/2) + 1
+    s_middle_one = round(sec_len/2) + 1     # if it is even it takes first middle one
 
+
+# Concatinating all the characters
 output = str(first_input[0]) + str(second_input[0]) + str(first_input[middle_one-1])+ str(second_input[s_middle_one-1])+str(first_input[-1]) + str(second_input[-1])
-print(output)
+
+print(output)       #print the output
--- second_program.py
+++ second_program.py
@@ -1,14 +1,14 @@
 
-def words_input():
-     c=0 
-     file=open("input.txt",'r') 
-     lines = file.read() 
-     words = lines.split() 
-     print(words)
-     for i in words: 
-         if len(i)<5: 
-             print(i) 
-             file.close()
+def words_input():              # Defining the function to read the file and checking the length of the words
+     c=0                        # Assaigning c to 0
+     file=open("input.txt",'r') # opening the input text file
+     lines = file.read()        # reading the text file  
+     words = lines.split()      # using the split function words are splitted into list
+
+     for i in words:            # iterating the list 
+         if len(i)<5:           # Checking the length of the each word and if len is less than five or not
+             print(i)           # printing the words which are less than 5
+             file.close()       # Closing the file
 
 
-words_input()
+words_input()      # Calling the function
