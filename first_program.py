9ef29485990f10d777c60133beaf8f37e63f4e9e
SravaniJalibili364 <89896513+SravaniJalibili364@users.noreply.github.com>
2021-09-16 15:29:23 +0530

    Update first_program.py

--- first_program.py
+++ first_program.py
@@ -6,9 +6,9 @@ else:
     middle_one = round(first_len/2) + 1         # if it is even it takes first middle one
 
 
-second_input = input()          # Taking the first String
-sec_len = len(second_input)     # checking the length of the first string
-if(sec_len % 2 != 0):           # if length of the first string is odd or not
+second_input = input()          # Taking the second String
+sec_len = len(second_input)     # checking the length of the second string
+if(sec_len % 2 != 0):           # if length of the second string is odd or not
     s_middle_one = round((sec_len/2))   #if it is odd print the middle one
 else:
     s_middle_one = round(sec_len/2) + 1     # if it is even it takes first middle one
